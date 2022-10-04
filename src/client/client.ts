7708543d3ed18cd5ce962414f5604d6b31904231
Volkan Ongun <volkan.ongun@afiniti.com>
2022-10-04 22:46:08 +0300

    base toon and outlineeffect added

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,9 +2,17 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module.js'
 import {GUI} from 'dat.gui'
+import {OutlineEffect} from 'three/examples/jsm/effects/OutlineEffect'
 
 const scene = new THREE.Scene()
 scene.background = new THREE.Color(0xFFFFFF)
+
+const light = new THREE.PointLight(0xffffff, .5)
+light.position.set(3, 3, 3)
+scene.add(light)
+
+const ambientLight = new THREE.AmbientLight( 0xFFFFFF, .5 );
+scene.add( ambientLight );
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -12,7 +20,7 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.z = 2
+camera.position.z = 3.5
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -20,14 +28,31 @@ document.body.appendChild(renderer.domElement)
 
 new OrbitControls(camera, renderer.domElement)
 
+const outline = new OutlineEffect( renderer, {
+    defaultThickness: .01,
+    defaultColor: [ .55, 0, 0 ],
+    defaultAlpha: .8,
+    defaultKeepAlive: true // keeps outline material in cache even if material is removed from scene
+});
+
+const fiveTone = new THREE.TextureLoader().load('img/gradientMaps/fiveTone.jpg')
+fiveTone.minFilter = THREE.NearestFilter
+fiveTone.magFilter = THREE.NearestFilter
+
 const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x000000,
-    wireframe: true,
+const material: THREE.MeshToonMaterial = new THREE.MeshToonMaterial({
+    color: 0xFF0000,
+    gradientMap: fiveTone,
 })
 
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
+cube.position.x=-1
+
+const sphereGeometry = new THREE.SphereGeometry()
+const sphere = new THREE.Mesh(sphereGeometry, material)
+sphere.position.x = 1
+scene.add(sphere)
 
 window.addEventListener('resize', onWindowResize, false)
 
@@ -47,6 +72,12 @@ cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2)
 cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2)
 cubeFolder.open()
 
+const lightFolder = gui.addFolder('light')
+lightFolder.add(light, "intensity", 0, 1, .01)
+lightFolder.add(light.position, "x", -5, 5, .01)
+lightFolder.add(light.position, "y", -5, 5, .01)
+lightFolder.add(light.position, "z", -5, 5, .01)
+
 const cameraFolder = gui.addFolder('camera')
 cameraFolder.add(camera.position, 'z', 0, 20)
 cameraFolder.open()
@@ -55,10 +86,11 @@ function animate() {
 
     stats.update()
 
-    // cube.rotation.x += 0.01
-    // cube.rotation.y += 0.01
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
 
-    renderer.render(scene, camera)
+    outline.render(scene, camera)
+
 }
 
 renderer.setAnimationLoop(animate)
